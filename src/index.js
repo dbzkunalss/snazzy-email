112c9c1c2be723252adbf3265b13e1f48fc7babd
Kunal Sharma <kunalsharma.test@gmail.com>
2024-10-15 02:15:33 +0530

    feat:postal mime setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
+import PostalMime from 'postal-mime';
+
 export default {
-	async email(message, env, ctx) {
-  
-	}
-  }
+  async email(message, env, ctx) {
+    const email = await PostalMime.parse(message.raw);
+
+    console.log('Subject', email.subject);
+    console.log('HTML', email.html);
+
+    email.attachments.forEach((attachment) => {
+      let decoder = new TextDecoder('utf-8');
+      console.log('Attachment', attachment.filename, 
+        decoder.decode(attachment.content));
+    });
+  },
+};
